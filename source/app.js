07350416b445b53e23028ca233514e7e0c24e0b9
David Medina <demacdc6@gmail.com>
2022-09-07 23:03:35 -0300

    back end 1

--- source/app.js
+++ source/app.js
@@ -3,9 +3,16 @@ const path= require('path');
 const express=require('express');
 const app=express();
 
-app.get('/login',function(req,res){
-    res.send('LOGIN/login.html')}
+app.get('/',(req,res)=>{
+    res.send('ESTOY EN HOME')}
 )
-
-
-app.listen(3000)
+app.get('/detalles',(req,res)=>{
+    res.send('estoy en los productos detallados')
+})
+app.get('/login',(req,res)=>{
+    res.send('estoy en el inicio de sesion')
+})
+app.get('/carrito',(req,res)=>{
+    res.send('estoy en el carrito')
+})
+app.listen(3000,()=>console.log('Servidor iniciado en elpuerto 3000'))
