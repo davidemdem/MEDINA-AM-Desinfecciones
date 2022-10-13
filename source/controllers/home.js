09212ab382b60350e127e4b81e183e5a2dbac7c6
David Medina <demacdc6@gmail.com>
2022-10-13 00:35:11 -0300

    create parcials

--- source/controllers/home.js
+++ source/controllers/home.js
@@ -1,12 +1,20 @@
 
 // renderizar la vista para cargar un producto//
-const renderUploadProductView=(req, res) =>{
+const renderHomeView=(req, res) =>{
     return res.render('home.ejs');
 } 
 
+const renderLoginView=(req,res)=>{
 
+    return res.render('login.ejs')
+}
+const renderDetailServiceView=(req,res) =>{
+    return res.render('servicios.ejs')
+}
 
 
 module.exports={
-    renderUploadProductView
+    renderHomeView,
+    renderLoginView,
+    renderDetailServiceView
 };
