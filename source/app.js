447d2ffb2b9a51f2548a7bb522eca46f5b0bd760
David Medina <demacdc6@gmail.com>
2022-12-30 01:17:03 -0300

    cors install

--- source/app.js
+++ source/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const cookie = require("cookie-parser");
+const cors = require('cors')
 
 db.sequelize
 .sync()
@@ -18,6 +19,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors())
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
